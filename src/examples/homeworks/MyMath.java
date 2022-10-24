8177c8725f6a0520283883bb0e42dc5526a38191
taras <dyshkotaras@gmail.com>
2022-10-24 23:12:00 +0300

    24.10.2022 #21-22

--- src/examples/homeworks/MyMath.java
+++ src/examples/homeworks/MyMath.java
@@ -1,17 +1,47 @@
 package examples.homeworks;
 
     public class MyMath {
-        public static int multiple(int a, int b) {
+
+        public static String makeReadable(int seconds) {
+            String HH = String.valueOf(seconds / 3600);
+            String MM = String.valueOf(seconds % 3600 / 60);
+            String SS = String.valueOf(seconds % 3600 % 60);
+            if (HH.length() == 1) HH = "0" + HH;
+            if (MM.length() == 1) MM = "0" + MM;
+            if (SS.length() == 1) SS = "0" + SS;
+            return String.format("%d:%d:%d",HH,MM,SS);
+        }
+
+        public static final double PI = 3.14;
+
+        public static double length(double radisus) {
+            return 2 * PI * radisus;
+        }
+
+        public static double area(double radius) {
+            return radius * radius * PI;
+        }
+
+        public static int sum(int ... numbers) {
+            int sum = 0;
+            for (int number:
+                 numbers) {
+                sum += number;
+            }
+            return sum;
+        }
+
+         public static int multiple(int a, int b) {
             return a * b;
         }
-        public static double multiple(double a, double b) {
+         public static double multiple(double a, double b) {
             return a * b;
         }
-        public static double multiple(double a) {
+         public static double multiple(double a) {
             return multiple(a,a);
         }
 
-        public static int multiple(int a) {
+         public static int multiple(int a) {
             return multiple(a,a);
         }
 
--- src/test/Main.java
+++ src/test/Main.java
@@ -12,10 +12,23 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Person person = new Person("Tanya", 21);
-        person.setAge(-656);
-        System.out.println(person.getName() + " " + person.getAge());
+        String[] employees = getEmployees();
+        String[] newArray = new String[employees.length + 1];
+        for (String a:
+             employees) {
+            System.out.println(a);
+        }
+    }
 
+
+    private static String[] getEmployees() {
+        String[] employees = new String[5];
+        employees[0] = "0";
+        employees[1] = "1";
+        employees[2] = "2";
+        employees[3] = "3";
+        employees[4] = "4";
+        return employees;
     }
 }
 
